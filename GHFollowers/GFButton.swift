a1f4a10aa30899d96a1d1963d888a1e9e347e574
mine rala <mine-rala@hotmail.com>
2021-10-15 00:11:01 +0300

    Created custom button and textField.

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,34 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Mine Rala on 14.10.2021.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.titleLabel?.text = title
+        configure()
+    }
+
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
